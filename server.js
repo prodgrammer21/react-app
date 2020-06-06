95bdef9c36ba651a73ec23ca9ae4aae0c8044633
Rodrick <rodrick.alcantara@medcheck.com.ph>
2020-06-06 19:06:22 +0800

    Add server for react

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+/* Middleware */
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/build')))
+
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '/build/index.html'))
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
