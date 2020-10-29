68543e8003e5b47ecb5b1ff557dd41f3853a5c14
Rodrick <rodrick.alcantara@gmail.com>
2020-10-29 11:30:00 +0800

    Update test vaRIABLE

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 /* Middleware */
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '/build')))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/build")));
 
-app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '/build/index.html'))
-})
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "/build/index.html"));
+});
 
+console.log("PROCESS.ENV: ", process.env);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-})
+  console.log(`Listening on port: ${PORT}`);
+});
