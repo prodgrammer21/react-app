b9d452a493d2256b521b3f2153c95adf3fd17602
Rodrick <rodrick.alcantara@gmail.com>
2020-10-29 12:13:10 +0800

    Update test variable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-dotenv.config();
 
 console.log("process.env: ", process.env);
 
